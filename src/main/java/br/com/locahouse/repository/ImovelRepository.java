6d5a3230959a6722f08a5cd230c46f3eba8805b7
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-09-28 14:40:16 -0300

    Criação da camada de persistência.

---
+++ src/main/java/br/com/locahouse/repository/ImovelRepository.java
@@ -0,0 +1,9 @@
+package br.com.locahouse.repository;
+
+import br.com.locahouse.model.Imovel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImovelRepository extends JpaRepository<Imovel, Integer> {
+}
---
+++ src/main/java/br/com/locahouse/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.locahouse.repository;
+
+import br.com.locahouse.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}

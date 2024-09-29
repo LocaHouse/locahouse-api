babb563121aca52c8b7a10be8a85bf3152b2a73a
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-09-29 20:48:37 -0300

    Criação do UsuarioMapper.

---
+++ src/main/java/br/com/locahouse/mapper/UsuarioMapper.java
@@ -0,0 +1,37 @@
+package br.com.locahouse.mapper;
+
+import br.com.locahouse.dto.usuario.UsuarioGetDto;
+import br.com.locahouse.dto.usuario.UsuarioPostDto;
+import br.com.locahouse.model.Usuario;
+
+import java.util.Collections;
+import java.util.Optional;
+
+public final class UsuarioMapper {
+
+    private UsuarioMapper() {
+    }
+
+    public static Usuario usuarioPostDtoToEntity(UsuarioPostDto dto) {
+        Usuario usuario = new Usuario();
+        usuario.setCpf(dto.cpf());
+        usuario.setSenha(dto.senha());
+        usuario.setNome(dto.nome());
+        usuario.setDataNascimento(dto.dataNascimento());
+        usuario.setEmail(dto.email());
+        usuario.setTelefone(dto.telefone());
+        return usuario;
+    }
+
+    public static UsuarioGetDto entityToUsuarioGetDto(Usuario usuario) {
+        return new UsuarioGetDto(
+                usuario.getId(),
+                usuario.getCpf(),
+                usuario.getNome(),
+                usuario.getDataNascimento(),
+                usuario.getEmail(),
+                usuario.getTelefone(),
+                Optional.ofNullable(usuario.getImoveis()).orElse(Collections.emptyList())
+        );
+    }
+}

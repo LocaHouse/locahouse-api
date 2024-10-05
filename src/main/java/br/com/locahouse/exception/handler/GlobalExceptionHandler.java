3240023b91363646e2675411c837d661a906b0ad
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-10-05 11:24:16 -0300

    Criação do método handleMethodArgumentNotValid no GlobalExceptionHandler do projeto.

--- src/main/java/br/com/locahouse/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/locahouse/exception/handler/GlobalExceptionHandler.java
@@ -1,65 +1,52 @@
 package br.com.locahouse.exception.handler;
 
 import br.com.locahouse.exception.*;
-import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.MessageSource;
 import org.springframework.http.*;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @Resource
-    private MessageSource messageSource;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    private HttpHeaders headers() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        return headers;
-    }
-
-    private ResponseError responseError(HttpStatus statusCode, String message) {
-        return new ResponseError(LocalDateTime.now(), "error", statusCode.value(), message);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, @NonNull HttpHeaders headers, @NonNull HttpStatusCode status, @NonNull WebRequest request) {
+        List<String> errors = e.getBindingResult().getFieldErrors().stream().map(error -> Objects.requireNonNull(error.getDefaultMessage())).toList();
+        ResponseError error = responseError(status, errors);
+        return handleExceptionInternal(e, error, headers, status, request);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleUnexpectedException(Exception e, WebRequest request) {
         String message = "Unexpected server error.";
         LOGGER.error(message, e);
-        ResponseError error = responseError(HttpStatus.INTERNAL_SERVER_ERROR, message);
+        ResponseError error = responseError(HttpStatus.INTERNAL_SERVER_ERROR, new ArrayList<>(Collections.singletonList(message)));
         return handleExceptionInternal(e, error, headers(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<Object> handleBusinessException(BusinessException e, WebRequest request) {
-        ResponseError error = responseError(e.getHttpStatus(), e.getMessage());
-        return handleExceptionInternal(e, error, headers(), e.getHttpStatus(), request);
+        ResponseError error = responseError(e.getHttpStatusCode(), new ArrayList<>(Collections.singletonList(e.getMessage())));
+        return handleExceptionInternal(e, error, headers(), e.getHttpStatusCode(), request);
     }
 
-    /*@ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleMethodArgumentNotValidExceptio(BusinessException e, WebRequest request) {
-        ResponseError error = responseError(e.getHttpStatus(), e.getMessage());
-        return handleExceptionInternal(e, error, headers(), e.getHttpStatus(), request);
-    }*/
-
-    @ExceptionHandler(UniqueConstraintVioladaException.class)
-    public ResponseEntity<Object> handleUniqueConstraintVioladaException(UniqueConstraintVioladaException e, WebRequest request) {
-        ResponseError error = responseError(e.getHttpStatus(), e.getMessage());
-        return handleExceptionInternal(e, error, headers(), e.getHttpStatus(), request);
+    private ResponseError responseError(HttpStatusCode httpStatusCode, List<String> message) {
+        return new ResponseError(LocalDateTime.now(), "error", httpStatusCode.value(), message);
     }
 
-    @ExceptionHandler(UsuarioMenorDeIdadeException.class)
-    public ResponseEntity<Object> handleUsuarioMenorDeIdadeException(UsuarioMenorDeIdadeException e, WebRequest request) {
-        ResponseError error = responseError(e.getHttpStatus(), e.getMessage());
-        return handleExceptionInternal(e, error, headers(), e.getHttpStatus(), request);
+    private HttpHeaders headers() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
     }
 }

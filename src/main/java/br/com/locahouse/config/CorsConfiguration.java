38c2f3f7a286593db7a393a998dc0faf763f27c9
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-10-20 23:39:53 -0300

    Pacote específico para configurações.

--- src/main/java/br/com/locahouse/config/CorsConfiguration.java
+++ src/main/java/br/com/locahouse/config/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package br.com.locahouse.cors;
+package br.com.locahouse.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;

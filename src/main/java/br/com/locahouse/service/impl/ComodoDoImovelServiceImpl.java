bc980860f142a93ac700a5ae8aea791d888b3b09
Vitor <vitrom0907@gmail.com>
2024-10-22 23:00:45 -0300

    criação do ComodoDoImovelServiceImpl.

--- src/main/java/br/com/locahouse/service/impl/ComodoDoImovelServiceImpl.java
+++ src/main/java/br/com/locahouse/service/impl/ComodoDoImovelServiceImpl.java
@@ -1,33 +1,21 @@
-/*package br.com.locahouse.service.impl;
+package br.com.locahouse.service.impl;
 
-import br.com.locahouse.exception.RecursoNaoEcontradoException;
 import br.com.locahouse.model.ComodoDoImovel;
 import br.com.locahouse.repository.ComodoDoImovelRepository;
 import br.com.locahouse.service.ComodoDoImovelService;
-import br.com.locahouse.service.ComodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ComodoDoImovelServiceImpl implements ComodoDoImovelService {
 
     private final ComodoDoImovelRepository repository;
 
-    private final ComodoService comodoService;
-
     @Autowired
-    public ComodoDoImovelServiceImpl(ComodoDoImovelRepository repository, ComodoService comodoService) {
+    public ComodoDoImovelServiceImpl(ComodoDoImovelRepository repository) {
         this.repository = repository;
-        this.comodoService = comodoService;
     }
 
     @Override
     public ComodoDoImovel salvar(ComodoDoImovel comodoDoImovel) {
-        //comodoDoImovel.setImovel("");
-        comodoDoImovel.setComodo(this.comodoService.buscarPeloId(comodoDoImovel.getId()));
-
-        if (!comodoService.verificarExistencia(comodoDoImovel.getId()))
-            throw new RecursoNaoEcontradoException("Cômodo");
-
         return this.repository.save(comodoDoImovel);
     }
 }
-*/

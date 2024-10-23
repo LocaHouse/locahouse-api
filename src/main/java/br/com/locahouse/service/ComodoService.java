8623f4c7e004962d89d7a845b00beb26648c1c0a
Vitor <vitrom0907@gmail.com>
2024-10-22 23:00:15 -0300

    criação do ComodoService.

--- src/main/java/br/com/locahouse/service/ComodoService.java
+++ src/main/java/br/com/locahouse/service/ComodoService.java
@@ -1,4 +1,4 @@
-/*package br.com.locahouse.service;
+package br.com.locahouse.service;
 
 import br.com.locahouse.model.Comodo;
 
@@ -6,4 +6,3 @@ public interface ComodoService {
 
     Comodo buscarPeloId(Integer id);
 }
-*/

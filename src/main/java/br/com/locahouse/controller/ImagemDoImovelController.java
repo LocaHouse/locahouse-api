979bfe460a4e4e6782e216b0cb5106858e7dfa3d
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2025-04-08 01:28:50 -0300

    Ajuste ImagemDoImovelController

--- src/main/java/br/com/locahouse/controller/ImagemDoImovelController.java
+++ src/main/java/br/com/locahouse/controller/ImagemDoImovelController.java
@@ -1,9 +1,7 @@
 package br.com.locahouse.controller;
 
-import br.com.locahouse.controller.dto.imagemdoimovel.ImagemDoImovelCadastroAndAtualizacaoDto;
 import br.com.locahouse.model.ImagemDoImovel;
 import br.com.locahouse.service.ImagemDoImovelService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +9,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-
-import static br.com.locahouse.mapper.ImagemDoImovelMapper.*;
 
 @RestController
 @RequestMapping("/api/v1/imagens-imoveis")
@@ -25,9 +21,9 @@ public class ImagemDoImovelController {
         this.service = service;
     }
 
-    @PostMapping
-    public ResponseEntity<Void> cadastrar(@PathVariable Integer imovelId, @Valid @RequestBody ImagemDoImovelCadastroAndAtualizacaoDto dto, MultipartFile imagem) {
-        ImagemDoImovel imagemDoImovel = this.service.cadastrar(imovelId, imagemDoImovelCadastroAndAtualizacaoToEntity(dto), imagem);
+    @PostMapping("/upload/{imovelId}")
+    public ResponseEntity<Void> cadastrar(@PathVariable Integer imovelId, @RequestParam("imagem") MultipartFile imagem) {
+        ImagemDoImovel imagemDoImovel = this.service.cadastrar(imovelId, imagem);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().replacePath("api/v1/imagens-imoveis/buscar/{id}").buildAndExpand(imagemDoImovel.getId()).toUri();
         return ResponseEntity.created(location).build();
     }

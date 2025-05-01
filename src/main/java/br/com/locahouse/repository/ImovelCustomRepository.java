bbca100d5d37cd369461ccad57aeb7821cdd0a5e
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2025-04-30 22:17:38 -0300

    StatusImovelEnum nas querys.

--- src/main/java/br/com/locahouse/repository/ImovelCustomRepository.java
+++ src/main/java/br/com/locahouse/repository/ImovelCustomRepository.java
@@ -1,10 +1,11 @@
 package br.com.locahouse.repository;
 
 import br.com.locahouse.model.Imovel;
+import br.com.locahouse.model.enums.StatusImovelEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ImovelCustomRepository {
 
-    Page<Imovel> buscar(Pageable pageable, Integer usuarioId, Integer status);
+    Page<Imovel> buscar(Pageable pageable, Integer usuarioId, StatusImovelEnum status);
 }
